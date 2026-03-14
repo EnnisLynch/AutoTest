4d61b1924a72c9e6d434b042d5edbd2655ace98a
Ennis Lynch <ennis.lynch@gmail.com>
2026-03-14 12:20:17 -0400

    Created initial tables

--- DB/Tables/customer.sql
+++ DB/Tables/customer.sql
@@ -1,13 +1,17 @@
+SET ANSI_NULLS ON
+GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE TABLE [dbo].[customer](
-	[id] [bigint] NOT NULL,
-	[name] [nvarchar](255) NOT NULL,
- CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-))
-GO
+IF OBJECT_ID('dbo.order', 'U') IS NULL 
+BEGIN
+	CREATE TABLE [dbo].[customer](
+		[id] [bigint] NOT NULL,
+		[name] [nvarchar](255) NOT NULL,
+	 CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED 
+	(
+		[id] ASC
+	))
+	GO
+END
 
 
